b42bb828ee0cb1c471626dd4ff9641ff1a9d6695
Jimmy Miller <jimmyhmiller@gmail.com>
2026-09-10 16:56:41 -0400

    generate JavaScript update expressions

---
+++ benchmarks/react-hir-syntax-fixtures/update-expressions.ts
@@ -0,0 +1,15 @@
+let count = 0;
+let object = { value: 1 };
+let values = [1, 2];
+
+++count;
+count++;
+--object.value;
+object.value--;
+++values[0];
+values[1]--;
+
+count
+++count;
+
+count+++values[0];
