598d2899c7a36ba1860a19a273f4a493c567f917
Jimmy Miller <jimmyhmiller@gmail.com>
2026-09-09 23:39:01 -0400

    Add in-process direct SSA frontend benchmark

---
+++ benchmarks/run-react-hir-direct.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -eu
+
+benchmark_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+project_dir=$(dirname -- "$benchmark_dir")
+scratch_dir=$(mktemp -d "${TMPDIR:-/tmp}/react-hir-direct.XXXXXX")
+trap 'rm -rf "$scratch_dir"' EXIT INT TERM
+
+coil_compiler=${COIL_COMPILER:-coil}
+c_compiler=${CC:-clang}
+
+"$c_compiler" -O3 -c "$benchmark_dir/benchmark_barrier.c" \
+  -o "$scratch_dir/benchmark_barrier.o"
+"$coil_compiler" build "$benchmark_dir/react-hir-direct.coil" --release \
+  --link-flag "$scratch_dir/benchmark_barrier.o" \
+  -o "$scratch_dir/react-hir-direct-benchmark"
+
+cd "$project_dir"
+"$scratch_dir/react-hir-direct-benchmark" "$@"
