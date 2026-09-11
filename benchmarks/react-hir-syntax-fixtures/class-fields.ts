9e4a678d4ddc46abd36b55a3b5a9ad6babb16370
Jimmy Miller <jimmyhmiller@gmail.com>
2026-09-10 22:40:47 -0400

    Generate class method regions

--- benchmarks/react-hir-syntax-fixtures/class-fields.ts
+++ benchmarks/react-hir-syntax-fixtures/class-fields.ts
@@ -4,4 +4,14 @@ class Fields extends Base {
   value = 1;
   optional?: string;
   definite!: number;
+
+  constructor(value: number) {
+    this.value = value;
+  }
+
+  add(amount: number, scale = 1): number {
+    return this.value + amount * scale;
+  }
+
+  empty() {}
 }
