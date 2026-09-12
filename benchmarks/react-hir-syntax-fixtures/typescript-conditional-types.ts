9fdcd44d4ff00013a0792f01e7fb7559fd2563f6
Jimmy Miller <jimmyhmiller@gmail.com>
2026-09-12 03:42:56 -0400

    Verify structured TypeScript operand kinds

--- benchmarks/react-hir-syntax-fixtures/typescript-conditional-types.ts
+++ benchmarks/react-hir-syntax-fixtures/typescript-conditional-types.ts
@@ -1,4 +1,5 @@
 type Element<T> = T extends readonly (infer U)[] ? U : never;
+type ConstrainedElement<T> = T extends readonly (infer U extends PropertyKey)[] ? U : never;
 type Nested<T> = T extends string ? number : T extends bigint ? boolean : unknown;
 type Keys<T> = keyof T;
 type Query = typeof runtimeValue;
